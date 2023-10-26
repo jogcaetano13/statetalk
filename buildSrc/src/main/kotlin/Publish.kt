33668cd374ecbdc5d4c7077417c94423c77bd75b
jogcaetano13 <jogcaetano13@gmail.com>
2023-10-26 17:32:39 +0100

    Core bug fix

--- buildSrc/src/main/kotlin/Publish.kt
+++ buildSrc/src/main/kotlin/Publish.kt
@@ -3,5 +3,5 @@ object Publish {
     const val ARTIFACT_CORE_ID = "communication-core"
     const val ARTIFACT_PAGING_ID = "communication-paging"
     const val GROUP_ID = "com.github.joel.libraries"
-    const val LIBRARY_VERSION = "1.2.0"
+    const val LIBRARY_VERSION = "1.2.1"
 }
--- communication-core/build.gradle.kts
+++ communication-core/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
-    id("java-library")
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.dokka")
     `maven-publish`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
