3a52a66b5782744a640bc805f0ad72a37c0bc541
KMP Bits <kmpbits@gmail.com>
2025-06-20 21:07:27 +0100

    Increment StateTalk version
    This commit updates the `LIBRARY_VERSION` to 1.2.2`.

--- buildSrc/src/main/kotlin/Publish.kt
+++ buildSrc/src/main/kotlin/Publish.kt
@@ -3,5 +3,5 @@ object Publish {
     const val ARTIFACT_CORE_ID = "statetalk-core"
     const val ARTIFACT_PAGING_ID = "statetalk-paging"
     const val GROUP_ID = "com.github.joel.libraries"
-    const val LIBRARY_VERSION = "1.2.1"
+    const val LIBRARY_VERSION = "1.2.2"
 }
